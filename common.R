8505760499c6811c00206a855831d0c04154d8a8
burlutskiy <burlutskiy.a.s@gmail.com>
2015-01-11 19:44:02 -0500

    read using numbers on minutes

--- common.R
+++ common.R
@@ -3,7 +3,9 @@
 ## author Alexey Burlutskiy
 loadElectricPowerConsumption <- function(path){
   library(data.table)
-  DT <- fread(paste("grep ^[12]/2/2007 ", path), na.strings = c("?", ""))
+  dtime <- difftime(as.POSIXct("2007-02-03"), as.POSIXct("2007-02-01"),units="mins")
+  rowsToRead <- as.numeric(dtime)
+  DT <- fread(path, skip="1/2/2007", nrows = rowsToRead, na.strings = c("?", ""))
   setnames(DT, colnames(fread(path, nrows=0)))
   DT$DateTime <- as.POSIXct(paste(DT$Date, DT$Time), format="%d/%m/%Y %H:%M:%S")
   return (DT)
